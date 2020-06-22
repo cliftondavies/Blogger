23309a7e97c0736799b394135d992833bc16a2d6
cliftondavies <clifton.davies@outlook.com>
2020-06-22 21:55:14 +0100

    Set Flash Messages for Create and Destroy Actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,12 +20,14 @@ class ArticlesController < ApplicationController
     # @article.title = params[:article][:title]
     # @article.body = params[:article][:body]
     @article.save
+    flash.notice = "Article '#{@article.title}' Created!"
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+    flash.notice = "Article '#{@article.title}' Deleted!"
     redirect_to articles_path
   end
 
