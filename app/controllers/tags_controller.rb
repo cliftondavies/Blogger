fffd25de3dcd43c762b557f4c384852b4e1b536a
cliftondavies <clifton.davies@outlook.com>
2020-06-24 00:41:34 +0100

    List All Tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,16 @@
 class TagsController < ApplicationController
+  def index
+    @tags = Tag.all
+  end
+
   def show
     @tag = Tag.find(params[:id])
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    flash.notice = "Article '#{@tag.name}' Deleted!"
+    redirect_to tags_path
   end
 end
