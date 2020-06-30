b3338f0cebf2831825f076733136dbf99194b026
cliftondavies <clifton.davies@outlook.com>
2020-06-30 20:28:06 +0100

    Fix Layout/SpaceBeforeBlockBraces in article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,7 +10,7 @@ class Article < ApplicationRecord
   end
 
   def tag_list=(tags_string)
-    tag_names = tags_string.split(',').collect{ |s| s.strip.downcase }.uniq
+    tag_names = tags_string.split(',').collect { |s| s.strip.downcase }.uniq
     new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
     self.tags = new_or_found_tags
   end
